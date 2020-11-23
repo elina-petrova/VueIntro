e38c2b35ae8ddf9df2a0e5b3434fcc1c0cab84e4
elina <e_petrova@fanshaweonline.ca>
2020-11-23 15:31:35 -0500

    vue stuff

--- js/main.js
+++ js/main.js
@@ -2,16 +2,29 @@
 
 (() => {
     let vue_vm = new Vue({
-        el:"#app",
+        // el:"#app",
         data: {
             message:"Hello from Vue!",
             anotherMessage: "sup",
+            removeAformat:true,
+            showBioData: false,
 
             collection: [
                 { name: "Justin", role: "coordinator", nickname: "nitsuJ"},
                 { name: "Joe", role: "prof", nickname: "super chill"},
                 { name: "Jarrod", role: "coordinator", nickname: "Teddy Bear"}
             ]
+        },
+        
+
+        mounted: function(){
+            console.log("Vue is mounted!");
+
+            this.collection.push({name: "Jarrod", role: "supermodel prof", nickname: "Zoolander"});
+        },
+
+        updated: function(){
+            console.log("Vue just updated the DOM");
         },
 
         methods: {
@@ -20,6 +33,11 @@
             },
             clickHeader(){
                 console.log("clicked on the header");
+            },
+            removeProf(target){
+                console.log("removeProf", target);
+                
+                this.showBioData = this.showBioData ? false :true
             }
         }
     }).$mount("#app"); //also connects Vue to your wrapper in HTML 
